902b95b2dd5d0ba9beb9673a533e27d390a5a047
vodoanminhhieuvn <vodoanminhhieuvn@gmail.com>
2022-04-26 19:58:32 +0700

    Change title

--- template/slotSetGen/slotSetGen.py
+++ template/slotSetGen/slotSetGen.py
@@ -2,16 +2,17 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
-
-class ActionHelloWorld(Action):
+class ActionExample(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_example"
 
     async def run(self, dispatcher: CollectingDispatcher,
                   tracker: Tracker,
                   domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        
         dispatcher.utter_message(text="Hello World!")
         return []
